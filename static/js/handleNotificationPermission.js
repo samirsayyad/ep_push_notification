43ace4571edafd4e0fddc5f565c2388d56ab9e67
Samir Sayyad <samir.saiad@gmail.com>
2022-04-12 13:22:44 +0430

    fix permission issue

--- static/js/handleNotificationPermission.js
+++
@@ -1,1 +0,0 @@
-export const handleNotificationPermission = async () => await Notification.requestPermission();
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,28 @@
 import subscribeUserToTopic from './firebase/subscribeUserToTopic';
-
 export const aceInitialized = () => {
   try {
-    subscribeUserToTopic(
-        clientVars.ep_push_notification.publicVapidKey,
-        clientVars.ep_push_notification.firebaseConfig, pad.getPadId(), pad.getUserId());
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+      alert('This browser does not support desktop notification');
+    } else if (Notification.permission === 'granted') {
+      subscribeUserToTopic(
+          clientVars.ep_push_notification.publicVapidKey,
+          clientVars.ep_push_notification.firebaseConfig, pad.getPadId(), pad.getUserId());
+    // eslint-disable-next-line brace-style
+    }
+    // Otherwise, we need to ask the user for permission
+    else {
+      Notification.requestPermission().then((permission) => {
+        console.log(permission,"per ")
+      // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          subscribeUserToTopic(
+              clientVars.ep_push_notification.publicVapidKey,
+              clientVars.ep_push_notification.firebaseConfig,
+              pad.getPadId(), pad.getUserId());
+        }
+      });
+    }
   } catch (e) {
     console.log('[ep_push_notification]:', e.message);
   }
