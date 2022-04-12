1bace028bf6a1edd2cdd5909805f386ee364a085
Samir Sayyad <samir.saiad@gmail.com>
2022-04-12 15:58:55 +0430

    fix  Converting circular structure to JSON issue

--- server/expressServer.js
+++ server/expressServer.js
@@ -13,7 +13,7 @@ exports.expressCreateServer = (hookName, context) => {
           const {registrationToken} = req.body;
           const result = await topicServices.subscribeTokenToTopic(registrationToken, padId);
           console.log('result', result);
-          res.status(201).send(result);
+          res.status(201).json(result.data || {});
         } catch (error) {
           console.log('[ep_push_notification]:', error.message);
 
@@ -30,7 +30,7 @@ exports.expressCreateServer = (hookName, context) => {
           const {padId} = req.params;
           const {title, body} = req.body;
           const result = await topicServices.sendMessageToTopic(title, body, padId);
-          res.status(201).json(result);
+          res.status(201).json(result.data || {});
         } catch (error) {
           console.log('[ep_push_notification]:', error.message);
 
