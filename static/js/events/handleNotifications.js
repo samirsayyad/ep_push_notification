a920d625392258da38953ab969d5a0bb2d48a195
Samir Sayyad <samir.saiad@gmail.com>
2022-05-09 13:04:38 +0430

    check alert replace of notify in mobile

--- static/js/events/handleNotifications.js
+++ static/js/events/handleNotifications.js
@@ -23,25 +23,26 @@ export const sendNotificationToUser = async (...data) => {
 };
 
 export const notifyMe = async (...data) => {
-  // Let's check if the browser supports notifications
-  if (!('Notification' in window)) {
-    console.log('This browser does not support desktop notification');
-    return;
-  }
+  alert(data.body);
+  // // Let's check if the browser supports notifications
+  // if (!('Notification' in window)) {
+  //   console.log('This browser does not support desktop notification');
+  //   return;
+  // }
 
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === 'granted') {
-    // If it's okay let's create a notification
-    new Notification(data.title, {body: data.body});
-  }
+  // // Let's check whether notification permissions have already been granted
+  // else if (Notification.permission === 'granted') {
+  //   // If it's okay let's create a notification
+  //   new Notification(data.title, {body: data.body});
+  // }
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission().then((permission) => {
-      // If the user accepts, let's create a notification
-      if (permission === 'granted') {
-        new Notification(data.title, {body: data.body});
-      }
-    });
-  }
+  // // Otherwise, we need to ask the user for permission
+  // else if (Notification.permission !== 'denied') {
+  //   Notification.requestPermission().then((permission) => {
+  //     // If the user accepts, let's create a notification
+  //     if (permission === 'granted') {
+  //       new Notification(data.title, {body: data.body});
+  //     }
+  //   });
+  // }
 };
